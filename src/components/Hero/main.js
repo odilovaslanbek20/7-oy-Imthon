0766e88c894def6ff649626e16ae97e3fd2754f0
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-05-08 19:45:05 +0500

    ""

---
+++ src/components/Hero/main.js
@@ -0,0 +1,37 @@
+const cardsData = [
+	{
+		id: 1,
+		body: "Welcome to GreenShop",
+		title: `Let’s Make a Better `,
+		title1: `Planet`,
+		text: `We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!`,
+		text1: `We are an online plant shop offering a wide range of cheap and trendy plants.`,
+		images: "tuvak.svg",
+		images1: "tuvak1.svg",
+		btnText: "SHOP NOW",
+	},
+	{
+		id: 2,
+		body: "Welcome to GreenShop",
+		title: "LET'S LIVE IN A BETTER ",
+		title1: "Planet",
+		text: "We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!",
+		text1: `We are an online plant shop offering a wide range of cheap and trendy plants.`,
+		images: "tuvak.svg",
+		images1: "tuvak1.svg",
+		btnText: "LET'S START",
+	},
+	{
+		id: 3,
+		body: "Welcome to GreenShop",
+		title: "LET'S OBSERVE A BETTER  ",
+		title1: "Planet",
+		text: "We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!",
+		text1: `We are an online plant shop offering a wide range of cheap and trendy plants.`,
+		images: "tuvak.svg",
+		images1: "tuvak1.svg",
+		btnText: "GET CREDITS",
+	}
+]
+
+export default cardsData
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Cards from '../../components/Cards/Cards'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
 
 function HomePages() {
 	return (
 		<>
 		 <Header/>
+		 <Hero/>
 		 <Cards/>
 		 <Footer/>
 		</>
